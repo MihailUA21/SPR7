48013f760b3a5225ed743b1ebf214f42e44c6362
Mykhailo Savchenko <147519967+MihailUA21@users.noreply.github.com>
2023-12-01 20:46:23 +0200

    Add files via upload

---
+++ SPR7.cpp
@@ -0,0 +1,54 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+class LoanHandler{
+protected:
+    LoanHandler* successor;
+public:
+    virtual void processLoan(int amount) = 0;
+    virtual ~ LoanHandler(){}
+    LoanHandler* setSuccessor(LoanHandler* _successor) {
+        successor = _successor;
+        return successor;
+    }
+};
+
+class Cashier : public LoanHandler {
+public:
+    void processLoan(int amount) {
+        if (amount <= 10000) {
+            cout << "Кассир схвалив кредит на суму " << amount << endl;
+        }
+        else if (successor != nullptr) {
+            successor->processLoan(amount);
+        }
+    }
+};
+
+class Manager : public LoanHandler {
+public:
+    void processLoan(int amount) {
+        if (amount>10000 && amount <= 150000) {
+            cout << "Менеджер схвалив кредит на суму " << amount << endl;
+        }
+        else if (successor != nullptr) {
+            successor->processLoan(amount);
+        }
+    }
+};
+
+class Director : public LoanHandler {
+public:
+    void processLoan(int amount) {
+        if (amount > 150000) {
+            cout << "Директор банку схвалив кредит на суму " << amount << endl;
+        }
+    }
+};
+
+int main()
+{
+    
+}
+
